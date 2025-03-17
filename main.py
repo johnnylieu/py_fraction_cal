1f2b9ef479f1f931c2d4e1c1a3028d8b4705982d
Johnny <JohnnyLieu@yahoo.com>
2025-03-16 18:51:55 -0700

    able to grab arguments

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+
+if len(sys.argv) > 1:
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"Argument {i}: {arg}")
+else:
+    print("No command line arguments were provided.")
